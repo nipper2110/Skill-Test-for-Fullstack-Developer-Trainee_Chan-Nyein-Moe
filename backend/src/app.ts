9b3f6c6008a32f112883b2eba1b04120d004ae41
Chan Nyein Moe <channyeinmoe@Chans-MacBook-Pro.local>
2026-06-14 23:06:48 +0700

    Fix CORS error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL ?? "http://localhost:5173",
+      "https://taskflow-frontend-bxpd.onrender.com", // Render URL
       "http://localhost:5173",
       "http://localhost:5174",
     ],
